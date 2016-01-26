fa016ae29dfae63d6d6d70238dd655e470ea6772
Mateusz Urban <mateusz.urban@ihs.com>
2016-01-26 15:50:44 +0100

    seems like working

--- FiszkiPlayer/Program.cs
+++ FiszkiPlayer/Program.cs
@@ -5,6 +5,7 @@ using System.Linq;
 using System.Text;
 using System.Text.RegularExpressions;
 using System.Threading.Tasks;
+using NAudio.Wave;
 
 namespace FiszkiPlayer
 {
@@ -12,8 +13,23 @@ namespace FiszkiPlayer
     {
         static void Main(string[] args)
         {
-            var idToFile = Directory.GetFiles(@"c:\a\samplemp3", "*.mp3", SearchOption.AllDirectories).ToDictionary(k => int.Parse(Regex.Match(Path.GetFileName(k), @"\d+").Value));
+            var waveOutDevice = new WaveOut();
 
+            var idToFile = Directory.GetFiles(@"c:\a\samplemp3", "*.mp3", SearchOption.AllDirectories).ToDictionary(k => int.Parse(Regex.Match(Path.GetFileName(k), @"\d+").Value));
+            while (true)
+            {
+                Console.WriteLine("Wprowadz numer nagrania");
+                var trackId = int.Parse(Console.ReadLine());
+
+                using (var audioFileReader = new AudioFileReader(idToFile[trackId]))
+                {
+                    waveOutDevice.Init(audioFileReader);
+                    waveOutDevice.Play();
+
+                    Console.ReadLine();
+                }
+            }
+            
         }
     }
 }
