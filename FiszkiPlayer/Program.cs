811f5674ca162095e441dcdaa0edd1f9511b5522
Mateusz Urban <mateusz.urban@ihs.com>
2016-01-26 15:43:25 +0100

    initial commit

---
+++ FiszkiPlayer/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace FiszkiPlayer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var idToFile = Directory.GetFiles(@"c:\a\samplemp3", "*.mp3", SearchOption.AllDirectories).ToDictionary(k => int.Parse(Regex.Match(Path.GetFileName(k), @"\d+").Value));
+
+        }
+    }
+}
